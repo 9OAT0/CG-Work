d1b43acdced7376a57a0f9b59056e3e6df6a2c8a
Ykgzx <duu75512@gmail.com>
2025-06-23 02:51:46 +0700

    gg

---
+++ src/app/(website)/register/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+
+export default function Register() {
+    return (
+        <>
+        <Navbar />
+        <div>
+            hkodfjhfkotdg
+        </div>
+        </>
+    )
+}
