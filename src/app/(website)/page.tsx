ef2365822a4da1612d5513145a6c41b23e36bee0
Ykgzx <duu75512@gmail.com>
2025-07-16 15:23:09 +0700

    gg

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,52 +1,18 @@
-<<<<<<< Updated upstream
 'use client';
 
-import { useState, useEffect } from 'react';
 
-export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [user, setUser] = useState<any>(null);
-  const [error, setError] = useState('');
-
-  // ตรวจสอบ session ทันทีที่โหลดหน้า
-  useEffect(() => {
-    fetch('/api/me', { credentials: 'include' })
-      .then(res => res.json())
-      .then(data => {
-        if (data.user) setUser(data.user);
-      });
-  }, []);
-
-  const handleLogin = async () => {
-    const res = await fetch('/api/login', {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username }),
-    });
-    const data = await res.json();
-    if (res.ok) {
-      setUser(data.user);
-      console.log(data.user);
-      setError('');
-    } else {
-      setError(data.error || 'Login failed');
-    }
-  };
-
-  const handleLogout = async () => {
-    await fetch('/api/logout', { method: 'POST', credentials: 'include' });
-    setUser(null);
-  };
-
-=======
 export default function Home() {
->>>>>>> Stashed changes
+ 
   return (
     <>
-      <div className="min-h-screen bg-pink-500 flex justify-center items-center">
-        <img src="/Ellipse 2.png" alt="" />
+      <div
+      className="h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center"
+      style={{ backgroundImage: "url('/Rectangle 140.png')" }}
+    >
+      <div className=''>
+        <img src="/Ellipse 51.png" alt="" className='w-[291px] h-[291px]'/>
       </div>
+    </div>
     </>
   )
 }
---
+++ src/app/(website)/prelogin/page.tsx
@@ -0,0 +1,19 @@
+export default function PreloginPage() {
+    return (
+        <>
+            <div
+                className="h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center"
+                style={{ backgroundImage: "url('/Rectangle 140.png')" }}
+                >
+                <div className=''>
+                    <img src="/Ellipse 51.png" alt="" className='w-[291px] h-[291px]'/>
+                    <div>
+                        <button className="w-[250px] h-[49px] ">
+                            dgdfg
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
