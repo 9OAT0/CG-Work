db22e56d1460978dd10f16c6386672cedefc27a8
9OAT0 <oathaha30@gmail.com>
2025-07-16 22:14:50 +0700

    update

---
+++ src/lib/time.ts
@@ -0,0 +1,4 @@
+export function getThailandTime(date = new Date()): Date {
+    const offsetMs = 7 * 60 * 60 * 1000 // GMT+7 offset in milliseconds
+    return new Date(date.getTime() + offsetMs)
+  }  
