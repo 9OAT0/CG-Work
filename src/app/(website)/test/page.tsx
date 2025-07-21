831c74ef09f7ee2a8e14bfc9646c7b5be4780107
Ykgzx <duu75512@gmail.com>
2025-07-22 02:01:57 +0700

    gg

--- src/app/(website)/test/page.tsx
+++ src/app/(website)/test/page.tsx
@@ -3,7 +3,7 @@ export default function TestPage() {
         <>
             <div className="min-h-screen flex flex-col justify-center items-center gap-8">
                 <h1 className="text-4xl font-bold">กำลังข้ามมองสร้างไปก่อน</h1>
-                <a href="/homepage"><h1 className="text-4xl font-bold">Back</h1></a>
+                <a href="/homepage"><h1 className="text-4xl font-bold text-red-600">Getout!!!</h1></a>
             </div>
         </>
     )
